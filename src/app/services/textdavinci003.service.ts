ff3fd178d94eaf5a6599e59e1fcffe700172e39c
adsoftsito <adsoft@live.com.mx>
2023-03-15 16:07:16 -0600

    Develop (#11)
    * Adsoftsito patch 2 (#3)
    
    * create WF release (#1)
    
    * create develop WF
    
    * create media issue Closes #4 (#5)
    
    * create media issue #4
    
    * fix tests #4
    
    * create issue desv_standard #6 (#7)
    
    * add key #5 (#9)
    
    * add key #5
    
    * fix test #5
    
    * remove release WF

--- src/app/services/textdavinci003.service.ts
+++ src/app/services/textdavinci003.service.ts
@@ -17,7 +17,7 @@ apiURL = 'https://api.openai.com/v1/completions';
 httpOptions = {
   headers : new HttpHeaders( {
     'Content-Type' : 'application/json',
-    'Authorization': 'Bearer <key-here>',
+    'Authorization': 'Bearer ' + process.env['NG_APP_KEY'],
   }) 
 }
 
